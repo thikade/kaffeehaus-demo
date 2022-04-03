f286bda0dc4568fe98e5b9965f04708961de133b
thikade <thomas.hikade@gmail.com>
2022-04-03 14:40:10 +0000

    removed build and switch to openshift image

--- deployment/build-openshift.sh
+++
@@ -1,7 +0,0 @@
-# use <git-repo-URL>#BRANCH to specify the Git repo
-$ oc new-build  https://github.com/sdaschner/kaffeehaus-demo#main --name kaffeehaus  --name kaffeehaus -o yaml --dry-run  | tee buildconfig.yaml
-
-$ oc apply -f buildconfig.yaml
-
-# make K8s deployments use our imagestream
-$ oc set image-lookup kaffeehaus
